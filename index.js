d5f582cd62966464cc2f8fb853b31733fe442113
Jishnu <jishnu@waitwhile.com>
2021-02-15 15:56:32 +0530

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+export const fn1 = () => { console.log('this is from fn1') };
+export const fn2 = () => { console.log('this is from fn2') };
+export const fn3 = () => { console.log('this is from fn3') };
+export const fn4 = () => { console.log('this is from fn4') };
